f70184b9d8e3bba0b56d17a87decfb80beb59d91
Nick Steele <nick@grandst.com>
2013-07-16 19:01:43 +0000

    Buttons left, issue with admin_panel

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -119,24 +119,18 @@ if ( ! function_exists( 'grandscheme_posted_on' ) ) :
  */
 function grandscheme_posted_on() {
 	if ( is_sticky() ) {
-		printf( __( '<a href="%1$s" title="%2$s" rel="bookmark">%3$s</a><span class="byline"><span class="sep"> | </span><span class="author vcard"><a class="url fn n" href="%4$s" title="%5$s" rel="author">%6$s</a></span></span>', 'grandscheme' ),
+		printf( __( '<a href="%1$s" title="%2$s" rel="bookmark">%3$s</a>', 'grandscheme' ),
 			esc_url( get_permalink() ),
 			esc_attr( get_the_time() ),
-			esc_attr( __( 'Featured', 'grandscheme' ) ),
-			esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
-			esc_attr( sprintf( __( 'View all posts by %s', 'grandscheme' ), get_the_author() ) ),
-			esc_html( get_the_author() )
+			esc_attr( __( 'Featured', 'grandscheme' ) )
 		);
 	}
 	else {
-		printf( __( '<a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s" pubdate>%4$s</time></a><span class="byline"><span class="sep"> | </span><span class="author vcard"><a class="url fn n" href="%5$s" title="%6$s" rel="author">%7$s</a></span></span>', 'grandscheme' ),
+		printf( __( '<a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s" pubdate>%4$s</time></a>', 'grandscheme' ),
 			esc_url( get_permalink() ),
 			esc_attr( get_the_time() ),
 			esc_attr( get_the_date( 'c' ) ),
-			esc_html( get_the_date() ),
-			esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
-			esc_attr( sprintf( __( 'View all posts by %s', 'grandscheme' ), get_the_author() ) ),
-			esc_html( get_the_author() )
+			esc_html( get_the_date() )
 		);
 	}
 }
